67aa5e311f15e490e1a2646ec9c2f6399e095a87
R. Zander <r-zander@users.noreply.github.com>
2024-12-26 02:59:37 +0100

    [Index] [Mobile] Let first click unfold a tile instead of following its link.

--- themes/portfolio-theme/js/index.js
+++ themes/portfolio-theme/js/index.js
@@ -1,21 +1,47 @@
-$(function(){
+function syncHoverOfAboutMeLinks() {
 	/*
-	 * Connect the hover of both 'About Me' links
-	 */
+     * Connect the hover of both 'About Me' links
+     */
 	var link1 = $('#aboutMe');
 	var link2 = $('.contactLink');
 
-	link1.hover(function(){
-		link2.addClass("hover");
-	}, function(){
-		link2.removeClass("hover");
+	link1.hover(function () {
+		link2.addClass('hover');
+	}, function () {
+		link2.removeClass('hover');
 	});
 
-	link2.hover(function(){
-		link1.addClass("hover");
-	}, function(){
-		link1.removeClass("hover");
+	link2.hover(function () {
+		link1.addClass('hover');
+	}, function () {
+		link1.removeClass('hover');
 	});
 
-	initializeVideoAnimation(".subItems > a");
+	initializeVideoAnimation('.subItems > a');
+}
+
+function preventClicksToFoldedCategories() {
+	document.querySelectorAll('#indexNavigation .itemWrapper > a.categoryLink').forEach((link) => {
+		link.addEventListener('click', (event) => {
+			const listItem = link.closest('li');
+			if (!listItem.classList.contains('active')) {
+				document.querySelectorAll('#indexNavigation > li.active').forEach((activeListItem) => {
+					activeListItem.classList.remove('active');
+				});
+				listItem.classList.add('active');
+				event.preventDefault();
+			}
+			// else normally execute the click
+		});
+	});
+}
+
+$(function () {
+
+	syncHoverOfAboutMeLinks();
+
+	if (window.DetectIt.primaryInput !== 'touch') {
+		return;
+	}
+	preventClicksToFoldedCategories();
 });
